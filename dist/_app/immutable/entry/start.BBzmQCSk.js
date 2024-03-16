461cfd456acea6dd790ee409e1f75e5c6a949406
Lucas Börjesson <lucas.ri.borjesson@gmail.com>
2024-03-16 15:04:33 +0100

    Fix width issue

---
+++ dist/_app/immutable/entry/start.BBzmQCSk.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dxg7sX56.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
   import DrinkView from '$lib/views/DrinkView.svelte';
 </script>
 
-<div class="p-4 -m-2 flex space-between-4 flex-wrap">
+<div class="p-4 -m-2 flex space-between-4 flex-wrap w-screen">
   {#each data as d, idx}
     <DrinkView drinkData={d}/>
   {/each}
